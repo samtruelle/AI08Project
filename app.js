2adbf998ab3ddb5b8fc8a370cfc55395e2f3db43
samtruelle <samuel.reymondet@gmail.com>
2017-01-10 09:44:08 +0100

    pray

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//global.jQuery = require('jquery');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var consume = require('./routes/consume');
